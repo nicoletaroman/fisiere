a7f6963be1cf2a3324a3a00998a72ad71830db4e
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2021-01-14 09:28:48 +0200

    Add files via upload

---
+++ problema.py
@@ -0,0 +1,26 @@
+with open("input.txt","r") as f:
+    sir=f.readline()
+nrm=0
+nr=0
+n=0
+ns=0
+for i in sir:
+    if ord(i) in range(65,91):
+        nrm+=1
+with open("litereA.txt","w") as f:
+    f.write(str(nrm))
+for i in sir:
+    if ord(i) in range(97,123):
+        nr+=1
+with open("litereB.txt","w") as f:
+    f.write(str(nr))
+for i in sir:
+    if ord(i) in range(49,58):
+        n+=1
+with open("cifre.txt","w") as f:
+    f.write(str(n))
+for i in sir:
+    if ord(i) in range(33,42):
+        ns+=1
+with open("caractere.txt","w") as f:
+    f.write(str(ns))
